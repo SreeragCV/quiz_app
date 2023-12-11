33d3c7983f823c9008aada2c8c6b421df6506f8a
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-12-11 14:09:45 +0530

    quiz completion and questions shuffled

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,17 +1,30 @@
 import React, { useEffect, useState } from "react";
 import QUESTIONS from "../src/questions.js";
+import img from '../src/assets/quiz-complete.png'
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = QUESTIONS.length === activeQuestionIndex;
+
+  if(quizIsComplete){
+    return (
+        <div id="summary">   
+            <img src={img} alt="" />
+            <h2>QUIZ COMPLETE</h2>
+        </div>  
+    )
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  shuffledAnswers.sort(() => Math.random() - 0.5)
 
   function handleClick(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-
-    const storedIds = JSON.parse(localStorage.getItem('selectedAnswers')) || []
-    localStorage.setItem('selectedAnswers', JSON.stringify([...storedIds, selectedAnswer]))
+    // const storedIds = JSON.parse(localStorage.getItem('selectedAnswers')) || []
+    // localStorage.setItem('selectedAnswers', JSON.stringify([...storedIds, selectedAnswer]))
   }
 
   return (
